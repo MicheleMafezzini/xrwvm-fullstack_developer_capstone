4fd8ca28df141e68252bb3e5a353e95849df4a1d
MicheleMafezzini <michelemafezzini@gmail.com>
2025-12-12 23:39:16 -0500

    adding login/logout

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,18 +1,49 @@
 # Uncomment the required imports before adding the code
 
-# from django.shortcuts import render
-# from django.http import HttpResponseRedirect, HttpResponse
-# from django.contrib.auth.models import User
-# from django.shortcuts import get_object_or_404, render, redirect
-# from django.contrib.auth import logout
-# from django.contrib import messages
-# from datetime import datetime
+from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404, render, redirect
+from django.contrib.auth import logout
+from django.contrib import messages
+from datetime import datetime
 
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
 import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
+
+from django.contrib.auth import logout
+from django.http import JsonResponse
+
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+from django.http import JsonResponse
+import json
+
+def register_user(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        username = data.get("userName")
+        password = data.get("password")
+        email = data.get("email")
+        first_name = data.get("firstName")
+        last_name = data.get("lastName")
+
+        if User.objects.filter(username=username).exists():
+            return JsonResponse({"status": False, "error": "Already Registered"})
+
+        user = User.objects.create_user(username=username,
+                                        password=password,
+                                        email=email,
+                                        first_name=first_name,
+                                        last_name=last_name)
+        login(request, user)
+        return JsonResponse({"status": True, "userName": username})
+    return JsonResponse({"status": False, "error": "Invalid Method"})
+
+
 # from .populate import initiate
 
 
@@ -42,10 +73,49 @@ def login_user(request):
 # def logout_request(request):
 # ...
 
+def logout_user(request):
+    logout(request)
+    data = {"userName": ""}
+    return JsonResponse(data)
+
 # Create a `registration` view to handle sign up request
 # @csrf_exempt
 # def registration(request):
 # ...
+
+@csrf_exempt
+def registration(request):
+    context = {}
+
+    # Load JSON data from the request body
+    data = json.loads(request.body)
+    username = data['userName']
+    password = data['password']
+    first_name = data['firstName']
+    last_name = data['lastName']
+    email = data['email']
+    username_exist = False
+    email_exist = False
+    try:
+        # Check if user already exists
+        User.objects.get(username=username)
+        username_exist = True
+    except:
+        # If not, simply log this is a new user
+        logger.debug("{} is new user".format(username))
+
+    # If it is a new user
+    if not username_exist:
+        # Create user in auth_user table
+        user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,password=password, email=email)
+        # Login the user and redirect to list page
+        login(request, user)
+        data = {"userName":username,"status":"Authenticated"}
+        return JsonResponse(data)
+    else :
+        data = {"userName":username,"error":"Already Registered"}
+        return JsonResponse(data)
+
 
 # # Update the `get_dealerships` view to render the index page with
 # a list of dealerships
--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,18 @@
-import LoginPanel from "./components/Login/Login"
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home/Home";      // Assicurati di avere Home.jsx
+import LoginPanel from "./components/Login/Login";
+import Register from "./components/Register/Register";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<LoginPanel />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginPanel />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
   );
 }
+
 export default App;
